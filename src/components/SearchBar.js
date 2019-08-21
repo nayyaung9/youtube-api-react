eb419ec6ec43e91c45dd4ba8a5a7781b88e8e539
nayyaung9 <naygit.developer@gmail.com>
2019-08-21 22:24:32 +0630

    set onSubmit on SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,15 +4,22 @@ class SearchBar extends Component {
 
   state = { term: '' }
 
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    console.log(this.state.term)
+  }
 
   render() {
     return(
       <div>
-        <input  
-          type="text" 
-          value={this.state.term} 
-          onChange={e => this.setState({ term: e.target.value })}
-        />
+        <form onSubmit={this.onFormSubmit}>
+          <input  
+            type="text" 
+            value={this.state.term} 
+            onChange={e => this.setState({ term: e.target.value })}
+          />
+        </form>
       </div>
     )
   }
