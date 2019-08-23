8929ad3c1f81ba7392ccb5813196343e47473600
nayyaung9 <naygit.developer@gmail.com>
2019-08-23 13:23:13 +0630

    :fire: the dirty codes

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import VideoItem from './VideoItem';
 
+
+
 const VideoList = ({ videos, onVideoSelect }) => {
   const videoItems = videos.map((video) => {
     return (
-      <VideoItem 
-        video={video} 
-        key={video.id.videoId} 
-        onVideoSelect={onVideoSelect}
-      />
+      <div>
+     
+  
+      
+          <VideoItem 
+          video={video} 
+          key={video.id.videoId} 
+          onVideoSelect={onVideoSelect}
+        />
+     
+
+  
+    </div>
     )
   });
 
