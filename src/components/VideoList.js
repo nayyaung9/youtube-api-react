fed4fd60c28438085d51bd3c6bb8691dc75357bc
nayyaung9 <nayyaung.developer@gmail.com>
2019-08-23 11:43:52 +0630

    config VideoList

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos }) => {
   const videoItems = videos.map((video) => {
-    return <VideoItem video={video}/>
+    return <VideoItem video={video} key={video.id.videoId}/>
   });
 
   return <div>{videoItems}</div>
