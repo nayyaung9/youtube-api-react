052af04ffd7f60ca0b219fc42d7ee1e5fb80b4ff
nayyaung9 <naygit.developer@gmail.com>
2019-08-23 17:49:04 +0630

    config i:art: nline  styling components

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,6 +4,10 @@ import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
 
 class SearchBar extends Component {
 
@@ -24,18 +28,29 @@ class SearchBar extends Component {
   render() {
     return(
       <React.Fragment>
-        <AppBar position="fixed">
+        <AppBar position="fixed" color="inherit">
           <Toolbar>
-            <Typography>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" style={{ flexGrow: 1 }}>
               Youtube Player
             </Typography>
-            <form onSubmit={this.onFormSubmit}>
-              <input  
-                type="text" 
-                value={this.state.term} 
-                onChange={this.onInputChange}
-              />
+            <div style={{ position: 'relative', border: '1px solid #f1f1f1', borderRadius: '0.5em'}}>
+              <div style={{ height: '100%', position: 'absolute', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <SearchIcon />
+              </div>
+              <form onSubmit={this.onFormSubmit} style={{padding: 5 }}>
+                <InputBase 
+                  type="text" 
+                  placeholder="Search..."
+                  value={this.state.term} 
+                  onChange={this.onInputChange}
+                  style={{ marginLeft: 25 }}
+                />
             </form>
+            </div>
+            
           </Toolbar>
         </AppBar>
       </React.Fragment>
